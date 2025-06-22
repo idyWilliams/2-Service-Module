7cceaf7e45ebea635e89d88505e41faa64f00a3a
Williams <widorenyin0@gmail.com>
2025-06-22 20:39:48 +0100

    Add service module components

---
+++ screens/servicescreen.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import { Box, Text } from "@/components/Restyle";
+
+const ServiceScreen = () => {
+  return (
+    <Box alignItems="center" justifyContent="center" flex={1}>
+      <Text variant="bold16">Service Screen</Text>
+    </Box>
+  );
+}
+
+export default ServiceScreen
